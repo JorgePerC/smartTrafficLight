5cd1c12c9886c6ed7742afa3e17c3fb79c16f4c7
Jorge <jorgeperez_1@live.com>
2022-05-13 18:14:47 -0500

    codigo del rPi

--- pythonServer/TrafficLight.py
+++ pythonServer/TrafficLight.py
@@ -13,7 +13,9 @@ class TrafficLight :
     def turnOff(self) :
         try:  
             for led in self.ledPins.values():
-                digitalWrite(led, 0)   
+                digitalWrite(led, 0)
+            for k in self.ledStatus.keys():
+                self.ledStatus[k] = 0   
         except IOError:     # Print "Error" if communication error encountered            
             return ("Error trying to change state") 
         return "The semaphore is off"
@@ -22,14 +24,24 @@ class TrafficLight :
     def getColorStatus(self, color):
         return "Is the {} LED ON? {}".format(color, digitalRead(self.ledPins[color]))
 
+    def getColorStatus_(self, color):
+        return "Is the {} LED ON? {}".format(color, self.ledStatus[color])
+
+
     def getColor(self):
-        print(self.ledPins.keys())
         for k in self.ledPins.keys():
-            print(k, self.ledPins[k], digitalRead(self.ledPins[k]))
             if digitalRead(self.ledPins[k]) == 1:
                 return k #"The actual color is {}".format(color)
         return "Error, no color is turned on"
-        
+    
+    def getColor_(self):
+        for k in self.ledPins.keys():
+            #print(k, self.ledPins[k], digitalRead(self.ledPins[k]), self.ledStatus[k])
+            if self.ledStatus[k] == 1:
+                return k #"The actual color is {}".format(color)
+        return "Error, no color is turned on"
+
+
     # Function to turn only one LED at a time
     def setColor(self, color):
         try:
@@ -41,11 +53,28 @@ class TrafficLight :
         except IOError:  
             return ("Error trying to change state") 
         return "Set {} LED on.".format(color)
+    def setColor_(self, color):
+        try:
+            for i in self.ledPins:
+                if color == i:
+                    digitalWrite(self.ledPins[i], 1)
+                    self.ledStatus[i] = 1
+                else:
+                    digitalWrite(self.ledPins[i], 0)
+                    self.ledStatus[i] = 0
+        except IOError:  
+            return ("Error trying to change state") 
+        return "Set {} LED on.".format(color)
+
 
     def setColorStatus(self, color, status):
         # Update LED status 
         try:  
-            digitalWrite(self.ledPins[color], status)      
+            digitalWrite(self.ledPins[color], status)  
+            self.ledStatus[color] = status    
         except IOError:     # Print "Error" if communication error encountered            
             return ("Error trying to change state") 
         return "Set {} to: {}".format(color, status)
+
+if __name__ == "__main__":
+    a = TrafficLight(4, 3, 2)
